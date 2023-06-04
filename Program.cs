c8eda67cb84487a62eee1fa78bf3a4ea7902ea47
Evgheni Advahov <evghenii.88@gmail.com>
2023-06-04 19:09:31 +0300

    Excercise has been added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array = new string[] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[] {};
+
+for (int i = 0; i < array.Length; i++)
+{   
+    if(array[i].Length <=3)
+    {
+    Array.Resize(ref array2, array2.Length + 1 );
+    array2[array2.Length - 1] = array[i];
+    }
+}
+
+System.Console.WriteLine('['+ String.Join(',', array) + ']');
+System.Console.WriteLine('['+ String.Join(',', array2) + ']');
